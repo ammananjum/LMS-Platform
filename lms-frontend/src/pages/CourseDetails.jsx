e7ebf94ea1a4e7fac8338b6a77f8b33911b98ecd
ammananjum <emaananjum634@gmail.com>
2025-08-18 11:15:29 +0500

    Update CourseDetails.jsx

--- lms-frontend/src/pages/CourseDetails.jsx
+++ lms-frontend/src/pages/CourseDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link } from "react-router-dom";
-import { supabase } from './supabaseClient';
+import { supabase } from '../supabaseClient';
 import courses from "../data/courses";
 
 
