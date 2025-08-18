bf2049caa053af0e643f42af905efa0c45cd5814
ammananjum <emaananjum634@gmail.com>
2025-08-18 11:16:36 +0500

    Update Quiz.jsx

--- lms-frontend/src/pages/Quiz.jsx
+++ lms-frontend/src/pages/Quiz.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { supabase } from './supabaseClient';
+import { supabase } from '../supabaseClient';
 
 function Quiz() {
   const { courseId } = useParams();
