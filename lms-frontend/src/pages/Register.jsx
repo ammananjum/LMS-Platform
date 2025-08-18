0467be9b449b99fc3e507bdd63556a928019fd31
ammananjum <emaananjum634@gmail.com>
2025-08-18 11:51:31 +0500

    Update Register.jsx

--- lms-frontend/src/pages/Register.jsx
+++ lms-frontend/src/pages/Register.jsx
@@ -1,7 +1,6 @@
 // src/pages/Register.jsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from '../supabaseClient';
 
 function Register() {
   const navigate = useNavigate();
